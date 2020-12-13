d0fc78d3b133633eb17b0529c5a1938763f1b4cd
David Kim <davidhk21@gmail.com>
2020-12-12 22:48:29 -0700

    took out other services

--- server/config/services.js
+++ server/config/services.js
@@ -42,24 +42,24 @@
 
 module.exports = {
 
-  service1: {
-    api: '/carousel/:productid/*',
-    url: 'http://52.14.74.216:9999',
-    bundle: 'bundle.js',
-  },
+  // service1: {
+  //   api: '/carousel/:productid/*',
+  //   url: 'http://52.14.74.216:9999',
+  //   bundle: 'bundle.js',
+  // },
   service2: {
     api: '/products/:id/*',
     url: 'http://3.101.76.104:5000/',
     bundle: 'bundle.js'
   },
-  service3: {
-    api: ['/products1', '/photos', '/photosBlack', '/photosRed' ],
-    url: 'http://13.58.3.114:1238/',
-    bundle: 'bundle.js'
-  },
-  service4: {
-    api: '/ratings/*',
-    url: 'http://ec2-18-222-190-75.us-east-2.compute.amazonaws.com:4444/',
-    bundle: 'bundle.js'
-  }
+  // service3: {
+  //   api: ['/products1', '/photos', '/photosBlack', '/photosRed' ],
+  //   url: 'http://13.58.3.114:1238/',
+  //   bundle: 'bundle.js'
+  // },
+  // service4: {
+  //   api: '/ratings/*',
+  //   url: 'http://ec2-18-222-190-75.us-east-2.compute.amazonaws.com:4444/',
+  //   bundle: 'bundle.js'
+  // }
 };
