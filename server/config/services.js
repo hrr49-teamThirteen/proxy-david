dd4add4fce55e66fd9fa23b05de3e98554ecdea1
David Kim <davidhk21@gmail.com>
2020-12-16 16:32:29 -0700

    have all services running

--- server/config/services.js
+++ server/config/services.js
@@ -42,24 +42,24 @@
 
 module.exports = {
 
-  // service1: {
-  //   api: '/carousel/:productid/*',
-  //   url: 'http://52.14.74.216:9999',
-  //   bundle: 'bundle.js',
-  // },
+  service1: {
+    api: ['/carousel/:productid/*', '/carousel'],
+    url: 'http://3.14.153.82:8081/',
+    bundle: 'bundle.js',
+  },
   service2: {
     api: '/products/:id/*',
-    url: 'http://52.53.249.7/',
+    url: 'http://13.57.184.36/',
+    bundle: 'bundle.js'
+  },
+  service3: {
+    api: '/initial/*',
+    url: 'http://ec2-52-91-251-182.compute-1.amazonaws.com/',
+    bundle: 'bundle.js'
+  },
+  service4: {
+    api: '/ratings/*',
+    url: 'http://ec2-18-224-69-90.us-east-2.compute.amazonaws.com/',
     bundle: 'bundle.js'
   }
-  // service3: {
-  //   api: ['/products1', '/photos', '/photosBlack', '/photosRed' ],
-  //   url: 'http://13.58.3.114:1238/',
-  //   bundle: 'bundle.js'
-  // },
-  // service4: {
-  //   api: '/ratings/*',
-  //   url: 'http://ec2-18-222-190-75.us-east-2.compute.amazonaws.com:4444/',
-  //   bundle: 'bundle.js'
-  // }
 };
